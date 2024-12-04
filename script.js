4ce7475a5e9d7cc838436240ac3172d9ca3d1fdf
Pedro Garcia <Coolcatdopedog@gmail.com>
2024-12-04 10:22:56 -0800

    created an array, set first player to X, and ensured game is active

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const squares = document.querySelectorAll('.game-square') /* Selects all elements with class 'game-square' */
-const restart = document.getElementById('button-play-again') /* Selects ID element 'button-play-again' and stores it */
-const xVictory = document.getElementById('scoreboard-x') /* Selects ID element 'scoreboard-x' and stores it */
-const oVictory = document.getElementById('scoreboard-o') /* Selects ID element 'scoreboard-o' and stores it */
+const squares = document.querySelectorAll('.game-square'); /* Selects all elements with class 'game-square' */
+const restart = document.getElementById('button-play-again'); /* Selects ID element 'button-play-again' and stores it */
+const xVictory = document.getElementById('scoreboard-x'); /* Selects ID element 'scoreboard-x' and stores it */
+const oVictory = document.getElementById('scoreboard-o'); /* Selects ID element 'scoreboard-o' and stores it */
+
+ let board = Array(9).fill(null); // Creates array with 9 elements set to null
+let currentPlayer = 'X'; // Sets 'currentPlayer' to "X"
+let game = true; // To ensure current game is active
