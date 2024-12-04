60818add72d528635ec8989b9d79aac0347d9b85
Pedro Garcia <Coolcatdopedog@gmail.com>
2024-12-04 10:10:36 -0800

    added notes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const squares = document.querySelectorAll('.game-square')
-const restart = document.getElementById('button-play-again')
-const xVictory = document.getElementById('scoreboard-x')
-const oVictory = document.getElementById('scoreboard-o')
+const squares = document.querySelectorAll('.game-square') /* Selects all elements with class 'game-square' */
+const restart = document.getElementById('button-play-again') /* Selects ID element 'button-play-again' and stores it */
+const xVictory = document.getElementById('scoreboard-x') /* Selects ID element 'scoreboard-x' and stores it */
+const oVictory = document.getElementById('scoreboard-o') /* Selects ID element 'scoreboard-o' and stores it */
