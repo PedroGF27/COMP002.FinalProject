02b1425d057d4bab94c26317947fb81c034959c0
Pedro Garcia <Coolcatdopedog@gmail.com>
2024-12-04 10:03:58 -0800

    added selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+const squares = document.querySelectorAll('.game-square')
+const restart = document.getElementById('button-play-again')
+const xVictory = document.getElementById('scoreboard-x')
+const oVictory = document.getElementById('scoreboard-o')
